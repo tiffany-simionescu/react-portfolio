4e0e3a99370faf6757188e20fa068056369f042e
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-05-30 01:43:12 -0400

    Added Redux Store

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -5,14 +5,32 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
+// Redux Dependencies
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { loadState } from './sessionStorage';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
 
 // React MDL
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 
+// Redux Store
+const persistedState = loadState();
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+
+const store = createStore(
+  reducer,
+  persistedState,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
