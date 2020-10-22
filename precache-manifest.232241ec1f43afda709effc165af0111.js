ab032bf18dcd4fcaaa49f6a77829c6be9fdb4b03
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2020-10-22 18:51:22 -0400

    Updates

---
+++ precache-manifest.232241ec1f43afda709effc165af0111.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f755bc1a5c06a65cdca9d99758d42e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0958c8e6a784998d677",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "b9367bd08afa4b8f04fe",
+    "url": "/static/css/main.e67e538e.chunk.css"
+  },
+  {
+    "revision": "c0958c8e6a784998d677",
+    "url": "/static/js/2.755ed266.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.755ed266.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9367bd08afa4b8f04fe",
+    "url": "/static/js/main.ccfeda48.chunk.js"
+  },
+  {
+    "revision": "eb2da8a6912a8c64fb47",
+    "url": "/static/js/runtime-main.555d898a.js"
+  },
+  {
+    "revision": "29754c6b5f55a2493b6582232726dd4b",
+    "url": "/static/media/avatar.29754c6b.jpg"
+  }
+]);
